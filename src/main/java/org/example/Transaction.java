eeb5c8904570f63effad3f91c73420b7d717c45b
horlandouni1 <orland20161@gmail.com>
2025-03-28 23:43:05 -0500

    se implementó la clase transaction

--- src/main/java/org/example/Transaction.java
+++ src/main/java/org/example/Transaction.java
@@ -1,4 +1,26 @@
 package org.example;
 
 public class Transaction {
+    private int id;
+    private String type;
+    private double amount;
+
+    public Transaction(int id, String type, double amount) {
+        this.id = id;
+        this.type = type;
+        this.amount = amount;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
 }
